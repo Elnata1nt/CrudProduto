98003296a3916055e1ccc5e3281ce827ff71b1a0
Elnata <elnata_correa@hotmail.com>
2023-10-31 03:40:28 -0400

    subindo todos os arquivos

--- NovoProjeto/Program.cs
+++ NovoProjeto/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using Course;
 
+namespace Couser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            Console.WriteLine();
+            Console.WriteLine(" Dados do produto: " + p );
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionados: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados" + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados" + p);
+        }
+    }
+}
